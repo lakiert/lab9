1b0e80f9b1ed6d8549b8d6c473fa93c5daa66467
lakiert <aleksandra.lakiert@gmail.com>
2021-04-29 13:35:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import xlrd
+import openpyxl
+
+# zad1
+# imiona = pd.ExcelFile('imiona.xlsx')
+# df = pd.read_excel(imiona, header=0)
+# print(df)
+# dzieci = df.groupby(['Rok']).agg({'Liczba':['sum']})
+# print(dzieci)
+# dzieci.plot()
+# plt.xticks(np.arange(2000, 2018, step=1))
+# plt.xticks(rotation=90)
+# plt.show()
