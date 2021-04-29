8c6f5ad2420cb21c2997d412e0ba4cdb0dfd7819
lakiert <aleksandra.lakiert@gmail.com>
2021-04-29 13:46:29 +0200

    added part 2

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ import openpyxl
 # plt.xticks(np.arange(2000, 2018, step=1))
 # plt.xticks(rotation=90)
 # plt.show()
+
+# zad2
+
+# imiona = pd.ExcelFile('imiona.xlsx')
+# df = pd.read_excel(imiona, header=0)
+# print(df)
+# dzieci = df.groupby(['Plec']).agg({'Liczba':['sum']})
+# print(dzieci)
+# wykres = dzieci.plot.bar()
+# wykres.set_xlabel('Plec')
+# wykres.set_ylabel('Mln')
+# wykres.legend()
+# plt.title('Liczba dzieci danej płci urodzonych w latach 2000-2017')
+# plt.xticks(rotation=0)
+# plt.yticks(np.arange(0, 4000000, step=500000))
+# plt.show()
+
+
